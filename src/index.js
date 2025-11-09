2abf18dd56af000817193ac9469c98cdb8479779
Jay Yu <yngcompany@gmail.com>
2025-11-09 12:28:49 +0900

    css import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // src/index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
+import './index.css';
 import App from "./App";
 import { HashRouter as Router } from "react-router-dom";
 
